2445ad1b40121d5c92248177b4f9b59b3b173717
RyoJerryYu <tzl19961216@gmail.com>
2025-02-10 17:00:42 +0800

    feat: add tsgenx

---
+++ pkg/codegen/tsgenx/import.go
@@ -0,0 +1,110 @@
+package tsgenx
+
+import (
+	"fmt"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"github.com/RyoJerryYu/go-utilx/pkg/container/containerx"
+	"github.com/RyoJerryYu/go-utilx/pkg/container/slicex"
+)
+
+type TSModule struct {
+	ModuleName string
+	Path       string // path relative to the generate root, or the absolute path
+	Relative   bool   // whether the path is relative to the current file
+}
+
+func (m TSModule) Ident(name string) TSIdent {
+	return TSIdent{
+		TSModule: m,
+		Name:     name,
+	}
+}
+
+func (m TSModule) AsIdent() TSIdent {
+	return TSIdent{
+		TSModule: m,
+	}
+}
+
+type TSIdent struct {
+	TSModule
+	Name string
+}
+
+func (m TSIdent) GetName() string {
+	return m.Name
+}
+
+func tsRelativeImportPath(thisPath string, modulePath string) (string, bool) {
+	thisDir := filepath.Dir(thisPath)
+	relativePath, err := filepath.Rel(thisDir, modulePath)
+	if err != nil {
+		return "", false
+	}
+	if !strings.Contains(relativePath, "/") && !strings.HasPrefix(relativePath, ".") {
+		relativePath = "./" + relativePath
+	}
+	return strings.TrimSuffix(relativePath, ".ts"), true
+}
+func (g *TSFileBuf) thisModulePath() string {
+	return g.Opts.GenFilePath
+}
+
+func (g *TSFileBuf) ImportSegments() string {
+	thisModulePath := g.thisModulePath()
+	var imports []string
+	modulePaths := make([]string, 0, len(g.ImportIdents))
+	for path := range g.ImportIdents {
+		modulePaths = append(modulePaths, path)
+	}
+	// sort by module import path
+	sort.Slice(modulePaths, func(i, j int) bool {
+		return modulePaths[i] < modulePaths[j]
+	})
+
+	for _, modulePath := range modulePaths {
+		idents := g.ImportIdents[modulePath]
+		module := idents[0].TSModule
+		importPath := module.Path
+		ok := false
+		if module.Relative {
+			importPath, ok = tsRelativeImportPath(thisModulePath, module.Path)
+			if !ok {
+				continue
+			}
+		}
+		imports = append(imports, g.importSegmentDirect(importPath, idents))
+	}
+	return strings.Join(imports, "\n")
+}
+
+func (g *TSFileBuf) importSegmentDirect(importPath string, idents []TSIdent) string {
+	if len(idents) == 0 {
+		return ""
+	}
+	identNames := containerx.ToNames(idents)
+	identNames = slicex.Deduplicate(identNames)
+	sort.Slice(identNames, func(i, j int) bool {
+		return identNames[i] < identNames[j]
+	})
+	importPackageNum := 0
+	for _, identName := range identNames {
+		if identName == "" {
+			importPackageNum += 1
+			break
+		}
+	}
+
+	importContents := []string{}
+	if importPackageNum > 0 {
+		importContents = append(importContents, idents[0].ModuleName)
+	}
+	if len(identNames) > importPackageNum {
+		importContents = append(importContents, fmt.Sprintf("{ %s }", strings.Join(identNames[importPackageNum:], ", ")))
+	}
+
+	return fmt.Sprintf("import %s from '%s';", strings.Join(importContents, ", "), importPath)
+}
---
+++ pkg/codegen/tsgenx/tsgenx.go
@@ -0,0 +1,77 @@
+package tsgenx
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type TSOption struct {
+	GenFileSuffix string
+	GenFilePath   string
+}
+
+type TSFileBuf struct {
+	buf          bytes.Buffer
+	Opts         TSOption
+	ImportIdents map[string][]TSIdent // map<module_path, TSIdent>
+}
+
+func NewTSFileBuf(opts TSOption) *TSFileBuf {
+	return &TSFileBuf{
+		Opts:         opts,
+		ImportIdents: make(map[string][]TSIdent),
+	}
+}
+
+func (g *TSFileBuf) Apply(w io.Writer) error {
+	if !strings.HasSuffix(g.Opts.GenFileSuffix, ".ts") {
+		_, err := io.Copy(w, &g.buf)
+		return err
+	}
+	content := g.buf.Bytes()
+	imports := g.ImportSegments()
+	res := fmt.Sprintf("%s\n\n%s", imports, content)
+	_, err := w.Write([]byte(res))
+	return err
+}
+
+func (g *TSFileBuf) Write(p []byte) (n int, err error) {
+	return g.buf.Write(p)
+}
+
+func (g *TSFileBuf) P(v ...any) {
+	for _, x := range v {
+		switch x := x.(type) {
+		case TSIdent:
+			fmt.Fprint(&g.buf, g.QualifiedTSIdent(x))
+		default:
+			fmt.Fprint(&g.buf, x)
+		}
+	}
+	fmt.Fprintln(&g.buf)
+}
+
+// Pf is same as P, but with formatted string.
+func (opt *TSFileBuf) Pf(format string, v ...any) {
+	newV := make([]any, len(v))
+	for i, x := range v {
+		switch x := x.(type) {
+		case TSIdent:
+			newV[i] = opt.QualifiedTSIdent(x)
+		default:
+			newV[i] = x
+		}
+	}
+	opt.P(fmt.Sprintf(format, newV...))
+}
+
+func (r *TSFileBuf) QualifiedTSIdent(ident TSIdent) string {
+	// glog.V(3).Infof("QualifiedTSIdent: %s", m.GoIdent.GoName)
+	if _, ok := r.ImportIdents[ident.Path]; !ok {
+		r.ImportIdents[ident.Path] = []TSIdent{}
+	}
+	r.ImportIdents[ident.Path] = append(r.ImportIdents[ident.Path], ident)
+	return ident.Name
+}
